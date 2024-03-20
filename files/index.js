0df5ad45b4e6d7446c857c1fed49a8e465fd9c9d
yeua6321 <140486106+yeua6321@users.noreply.github.com>
2024-03-20 21:48:03 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '3e6aa024-e4db-4fe5-9ca4-9d245c101eb5'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.zzx.free.hr';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOGI5NzI0MDgwZTU1ZTcwMzcwZmI3NDI4NzkyMmYzMWIiLCJ0IjoiOGNlY2VlYzQtYzZiNi00N2VkLThhZjItY2I4MThmMDkxZWJkIiwicyI6Ik5XWTFNV1ZsWm1NdFpEYzJZeTAwWkdSaExUbGtZall0TnpneVpqZ3haVE00WkRBNSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chosreo.dybo.x10.mx';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNjEwZmJmYzVkZDE3YTg3OGI3OTE4ZTRlZWQyMGFjZGUiLCJ0IjoiZWZhNTY2OTktZTE5NC00Nzk2LThkYTItNWQzOWUxMGFlOWE5IiwicyI6Ill6VTRZekJqWldZdE1qbGhOQzAwWldNM0xXSmtOR0l0Tmpaa1pUZGpPVGd4TnpoaCJ9';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
